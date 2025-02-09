190e79d64845b1577b62a1da7e67d455c658a558
NahidReza08 <shovo1999@gmail.com>
2025-02-09 16:18:00 +0600

    2.2.1 Derived State & Effects(New Approach)

--- src/lib/2_Reactivity/Effects.svelte
+++ src/lib/2_Reactivity/Effects.svelte
@@ -4,33 +4,31 @@
 -->
 
 <script>
-	let count = 1;
-    $: numbers = [...(numbers||[]), count];
+    let numbers = $state([1]);
+	let count = $derived(numbers.length);
+   
+    let total, multiplication, output;
 
-    let total, multiplication, output = {};
-
-    $:{
+    $effect(()=>{
         total = numbers.reduce((t, n) => t + n, 0)
-        multiplication = numbers.reduce((t, n) => t * n, 1)
-    }
+        multiplication = numbers.reduce((t, n) => t * n, 1);
+        formatOutput(numbers, total,multiplication)
+    });
 
     function formatOutput(numbers, total, multiplication){
         output = {
             total: `${numbers.join(' + ')} = ${total}`,
             multiplication: `${numbers.join(' * ')} = ${multiplication}`,
         }
-        console.log(output);
     }
-
-    $: formatOutput(numbers, total,multiplication);
 
 </script>
 
-<button type="button" class="btn btn-primary" onclick={()=>count++}>
+<button type="button" class="btn btn-primary" onclick={()=>numbers.push(count+1)}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
 
 <p>Numbers are: {numbers.join(' , ')}</p>
-<p>{output.total}</p>
-<p>{output.multiplication}</p>
+<p>{output?.total}</p>
+<p>{output?.multiplication}</p>
