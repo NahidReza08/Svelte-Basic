a33330e1a61a8d482f95ee2541c2a60a897e706f
NahidReza08 <shovo1999@gmail.com>
2025-02-09 13:10:48 +0600

    2.1 State

--- src/lib/2_Reactivity/State.svelte
+++ src/lib/2_Reactivity/State.svelte
@@ -4,14 +4,21 @@
 -->
 
 <script>
-	let count = 0;
-    let numbers = [];
+	let count = $state(0);
+    let numbers = $state([]);
 
     function handleClick(){
         count++;
         numbers.push(count);
-        numbers = numbers;
+        //console.log(":::count :::",$state.snapshot(count), count); //$state.snapshot is a shallow copy of count;
     }
+
+    //$inspect is a debugging tool, works with $state, $props, $derived, passed as multiple param
+    $inspect(numbers).with((type,numbersCurrentValue)=>{
+        console.log(type,numbersCurrentValue);
+        console.trace();
+    }); 
+
 </script>
 
 <button type="button" class="btn btn-primary" onclick={handleClick}>
