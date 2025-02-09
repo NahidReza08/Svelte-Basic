b0f7448ec3a929a3006e3b1bd9d42458d8f75e34
NahidReza08 <shovo1999@gmail.com>
2025-02-09 12:40:14 +0600

    2. Reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
-  import Introduction from './lib/1_Introduction/Introduction.svelte'
+  import Introduction from './lib/1_Introduction/Introduction.svelte';
+  import State from './lib/2_Reactivity/State.svelte';
+
 </script>
 
 <!-- Example of nested component. Loops not allowed. -->
 
-<Introduction />
+<!-- <Introduction /> -->
+<State />
---
+++ src/lib/2_Reactivity/State.svelte
@@ -0,0 +1,22 @@
+<!-- 
+    All let variable worked as reactive variable
+    Reactivity triggered in assignment
+-->
+
+<script>
+	let count = 0;
+    let numbers = [];
+
+    function handleClick(){
+        count++;
+        numbers.push(count);
+        numbers = numbers;
+    }
+</script>
+
+<button type="button" class="btn btn-primary" onclick={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>Numbers are: {numbers.join(' , ')}</p>
