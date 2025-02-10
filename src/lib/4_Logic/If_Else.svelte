749714d23acde2258103381f3a81f89a967cb274
NahidReza08 <shovo1999@gmail.com>
2025-02-10 17:16:04 +0600

    4.1 If Else Block

--- src/lib/4_Logic/If_Else.svelte
+++ src/lib/4_Logic/If_Else.svelte
@@ -3,6 +3,19 @@
   const number2 = 7;
 </script>
 
+<!-- ::: Example of If Block :::: -->
+{#if number1 > number2}
+  <p>{number1} is greater than {number2}</p>
+{/if}
+
+<!-- ::: Example of If-Else Block :::: -->
+{#if number1 > number2}
+  <p>{number1} is greater than {number2}</p>
+{:else}
+  <p>{number1} is not greater to {number2}</p>
+{/if}
+
+<!-- ::: Example of If-Else If-Else Block :::: -->
 {#if number1 > number2}
   <p>{number1} is greater than {number2}</p>
 {:else if number1 < number2}
